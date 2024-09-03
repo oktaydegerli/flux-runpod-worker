799b066d9b256772de8d588f6acf00e1a4b3c05f
Oktay Değerli <oktaydegerli@gmail.com>
2024-09-04 00:33:15 +0300

    Create model_fetcher.py

---
+++ builder/model_fetcher.py
@@ -0,0 +1,23 @@
+import torch
+import shutil
+from pathlib import Path
+from diffusers import DiffusionPipeline
+
+MODEL_ID = "black-forest-labs/FLUX.1-schnell"
+MODEL_CACHE_DIR = "diffusers-cache"
+
+def download_model():
+    model_cache_path = Path(MODEL_CACHE_DIR)
+    if model_cache_path.exists():
+        shutil.rmtree(model_cache_path)
+    model_cache_path.mkdir(parents=True, exist_ok=True)
+
+    DiffusionPipeline.from_pretrained(
+        MODEL_ID,
+        cache_dir=Path(MODEL_CACHE_DIR),
+        torch_dtype=torch.bfloat16
+    ).to('cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu')
+
+
+if __name__ == "__main__":
+    download_model()

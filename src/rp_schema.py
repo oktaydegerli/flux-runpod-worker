d0e7278175096d1b8cc16e091a62e57e05b0170c
Oktay Değerli <oktaydegerli@gmail.com>
2024-09-04 00:34:15 +0300

    Create rp_schema.py

---
+++ src/rp_schema.py
@@ -0,0 +1,41 @@
+INPUT_SCHEMA = {
+    'prompt': {
+        'type': str,
+        'required': True
+    },
+    'width': {
+        'type': int,
+        'required': False,
+        'default': 768,
+        'constraints': lambda width: width in [128, 256, 384, 448, 512, 576, 640, 704, 768, 832, 896, 960, 1024]
+    },
+    'height': {
+        'type': int,
+        'required': False,
+        'default': 768,
+        'constraints': lambda height: height in [128, 256, 384, 448, 512, 576, 640, 704, 768, 832, 896, 960, 1024]
+    },
+    'num_outputs': {
+        'type': int,
+        'required': False,
+        'default': 1,
+        'constraints': lambda num_outputs: 10 > num_outputs > 0
+    },
+    'num_inference_steps': {
+        'type': int,
+        'required': False,
+        'default': 50,
+        'constraints': lambda num_inference_steps: 0 < num_inference_steps < 500
+    },
+    'guidance_scale': {
+        'type': float,
+        'required': False,
+        'default': 7.5,
+        'constraints': lambda guidance_scale: 0 < guidance_scale < 20
+    },
+    'seed': {
+        'type': int,
+        'required': False,
+        'default': None
+    }
+}

0545d3377f98a043b0189b00070aa437a8907cec
Oktay Değerli <oktaydegerli@gmail.com>
2024-09-04 11:17:55 +0300

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -6,7 +6,7 @@ from typing import List
 from pathlib import Path
 
 MODEL_ID = "black-forest-labs/FLUX.1-schnell"
-MODEL_CACHE_DIR = "/runpod-volume/diffusers-cache/flux-schnell"
+MODEL_CACHE_DIR = "/runpod-volume/diffusers-cache/flux-dev"
 
 class Predictor:
     def setup(self):
